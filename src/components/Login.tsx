0f51fa81455c290c6a414b22653bc5d05e6b6f12
Muhammad Awais <m.awais2030@gmail.com>
2023-09-06 16:15:34 +0500

    Signin Complete

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,53 @@
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
 export const Login = () => {
-  return <div>Login</div>
+  return (
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+      <form>
+        <VStack
+          alignItems={'stretch'}
+          spacing={'8'}
+          w={['full', '96']}
+          m={'auto'}
+          my={'16'}
+        >
+          <Heading>Welcome Back</Heading>
+          <Input
+            placeholder='Email....'
+            type='email'
+            required={true}
+            focusBorderColor={'purple.500'}
+          />
+          <Input
+            placeholder='Password'
+            type='password'
+            required={true}
+            focusBorderColor={'purple.500'}
+          />
+
+          <Button variant={'link'} alignSelf={'flex-end'}>
+            <Link to={'/forgetpassword'}>Forget Password?</Link>
+          </Button>
+
+          <Button colorScheme={'purple'} type='submit'>
+            Login
+          </Button>
+          <Text textAlign={'right'}>
+            New User?{'  '}
+            <Button variant={'link'} colorScheme={'purple'}>
+              <Link to={'/signup'}> Sign Up</Link>
+            </Button>
+          </Text>
+        </VStack>
+      </form>
+    </Container>
+  )
 }
