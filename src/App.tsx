e9d300e0344114a7afb5420d23999a2d5e43308f
Muhammad Awais <m.awais2030@gmail.com>
2023-09-06 15:03:44 +0500

    Footer Component Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { SideBar, Home } from './components'
+import { SideBar, Home, Footer } from './components'
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { SideBar } from './SideBar'
 import { Home } from './Home'
+import { Footer } from './Footer'
 
-export { SideBar, Home }
+export { SideBar, Home, Footer }
