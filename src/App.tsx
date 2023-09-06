d523383dc9791790dcda6168e56f976e799e5ca8
Muhammad Awais <m.awais2030@gmail.com>
2023-09-06 15:54:20 +0500

    Further Cleanup and Added Login and Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { SideBar, Home, Footer, Videos, Upload } from './components'
+import {
+  SideBar,
+  Home,
+  Footer,
+  Videos,
+  Upload,
+  Login,
+  Signup,
+} from './components'
 
 const App = () => {
   return (
@@ -11,6 +17,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/videos' element={<Videos />} />
         <Route path='/upload' element={<Upload />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
       <Footer />
     </Router>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import { Home } from './Home'
 import { Footer } from './Footer'
 import { Upload } from './Upload'
 import { Videos } from './Videos'
+import { Login } from './Login'
+import { Signup } from './Signup'
 
-export { SideBar, Home, Footer, Videos, Upload }
+export { SideBar, Home, Footer, Videos, Upload, Login, Signup }
